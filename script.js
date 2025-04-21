f618dbfbabbf4e40a1c9ba2a7ab398a4c4bd9f87
Mandy Xiang <mandy_xiang@outlook.com>
2025-04-21 14:09:15 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// Toggle menu for small screens
+function myFunction() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+      x.className += " w3-show";
+    } else {
+      x.className = x.className.replace(" w3-show", "");
+    }
+   }
+
+   function myFunction() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+      x.className += " w3-show";
+    } else {
+      x.className = x.className.replace(" w3-show", "");
+    }
+  }
